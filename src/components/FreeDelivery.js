6de3008c4f0b569d5e01502f1b26353a485b6796
drasti1999 <drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.com>
2022-08-29 18:54:59 +0530

    till Free Delivery

--- src/components/FreeDelivery.js
+++ src/components/FreeDelivery.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function FreeDelivery() {
-  return (<div>
-  <h1>
-    Free Delivery
-  </h1>
-  </div>
-  )
+  return (
+    <div className="my-0 py-5 bg-site bg-free-delivery">
+      <div className="container">
+        <h1 className="text-center display-4 fw-bolder text-color">Free Delivery</h1>
+      </div>
+      <div className="d-flex justify-content-center my-2 pb-3">
+        <p className="text-color h5 mx-5 my-5 text-center">
+          Duis euismod vestibulum nunc, id convallis dui ullamcorper ut.
+          Vestibulum blandit dictum neque sit amet feugiat.
+          <br /> Duis euismod vestibulum nunc, id convallis dui ullamcorper ut.
+          Vestibulum blandit dictum neque sit amet feugiat.
+          <br />    Suspendisse eu molestie risus. Aenean eros purus, tincidunt at nibh
+          ut, volutpat egestas ipsum.
+          <br /> 
+          Suspendisse eu molestie risus. Aenean eros purus, tincidunt at nibh
+          ut, volutpat egestas ipsum.
+          <br /> molestie risus. Aenean eros purus, tincidunt at nibh.<br/> molestie risus. Aenean eros purus, tincidunt at nibh.<br/>
+         
+      
+        </p>
+      </div>
+      <div className="d-flex justify-content-center mt-5">
+          <button className="btn rounded-5 btn-site">Get Voucher</button>
+        </div>
+    </div>
+  );
 }
