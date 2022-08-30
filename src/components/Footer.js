aa3087a6c6253a142ed575d02dd8a961bb94e026
drasti1999 <drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.com>
2022-08-30 16:32:33 +0530

    footer design over

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,81 +2,79 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className=" text-white" style={{ paddingTop: "8rem" }}>
+    <div className="text-white" style={{ paddingTop: "8rem" }}>
       <img
         src="/assets/footerImage.png"
         className="h-100 w-100 position-absolute"
         alt=""
-        style={{ marginTop: " -25rem !important" }}
+        style={{ marginTop: "-30rem" }}
       />
       {/* <nav className="position-absolute navbar navbar-expand-lg bg-transparent navbar-dark fixed-top"> */}
-
-      <div className="container mt-5 position-relative" style={{ zIndex: 2 }}>
-        <div className="row p-5">
-          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <h4 className="h4 text-uppercase">About</h4>
-            <div className="mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              cursus odio massa, ut maximus ligula. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Praesent cursus odio massa, ut
-              maximus ligula
+      <div className="bg-site">
+        <div className="container position-relative" style={{ zIndex: 2 }}>
+          <div className="row py-5 px-2">
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-5 col-xs-12">
+              <h4 className="h4 text-uppercase text">About</h4>
+              <div className="mt-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent cursus odio massa, ut maximus ligula. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Praesent cursus odio
+                massa, ut maximus ligula
+              </div>
             </div>
-          </div>
-          <div className="col-xl-2 col-lg- col-md-2 col-sm-2 col-xs-2">
-            <h4 className="h4 text-uppercase">About</h4>
-            <div>
-              <ul className="list-group">
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Team</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Join Us</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Ethic</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Goals</a>
-                </li>
-              </ul>
+            <div className="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2 ps-3 w-custom-footer">
+              <h1 className="h4 text-uppercase">About</h1>
+              <a href="" className=" text-white text-decoration-none">
+                Team
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Join Us
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Ethic
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Goals
+              </a>
             </div>
-          </div>
-          <div className="col-xl-2 col-lg- col-md-2 col-sm-2 col-xs-2">
-            <h4 className="h4 text-uppercase">About</h4>
-            <div>
-              <ul className="list-group">
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Team</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Join Us</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Ethic</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Goals</a>
-                </li>
-              </ul>
+            <div className="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2 ps-3 w-custom-footer">
+              <h1 className="h4 text-uppercase">About</h1>
+              <a href="" className=" text-white text-decoration-none">
+                Team
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Join Us
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Ethic
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Goals
+              </a>
             </div>
-          </div>
-          <div className="col-xl-2 col-lg- col-md-2 col-sm-2 col-xs-2">
-            <h4 className="h4 text-uppercase">About</h4>
-            <div>
-              <ul className="list-group">
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Team</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Join Us</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Ethic</a>
-                </li>
-                <li className="list-group-item bg-transparent border-0">
-                  <a href="" className=" text-white text-decoration-none">Goals</a>
-                </li>
-              </ul>
+            <div className="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2 ps-3 w-custom-footer">
+              <h1 className="h4 text-uppercase">About</h1>
+              <a href="" className=" text-white text-decoration-none">
+                Team
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Join Us
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Ethic
+              </a>
+              <br />
+              <a href="" className=" text-white text-decoration-none">
+                Goals
+              </a>
             </div>
           </div>
         </div>
